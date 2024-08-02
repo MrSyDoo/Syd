b2be638bbf48432372eddf4bf62a75e890504fe3
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2024-08-02 16:24:13 +0530

    Update route.py

--- plugins/route.py
+++ plugins/route.py
@@ -6,12 +6,12 @@ import re, math, logging, secrets, mimetypes, time
 from info import *
 from aiohttp import web
 from aiohttp.http_exceptions import BadStatusLine
-from TechVJ.bot import multi_clients, work_loads, TechVJBot
-from TechVJ.server.exceptions import FIleNotFound, InvalidHash
-from TechVJ import StartTime, __version__
-from TechVJ.util.custom_dl import ByteStreamer
-from TechVJ.util.time_format import get_readable_time
-from TechVJ.util.render_template import render_page
+from MrSyD.bot import multi_clients, work_loads, TechVJBot
+from MrSyD.server.exceptions import FIleNotFound, InvalidHash
+from MrSyD import StartTime, __version__
+from MrSyD.util.custom_dl import ByteStreamer
+from MrSyD.util.time_format import get_readable_time
+from MrSyD.util.render_template import render_page
 
 routes = web.RouteTableDef()
 
