4ecee83b88640c6f80dac0513de1e2837bb6119c
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2024-08-04 08:35:53 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,6 +1,6 @@
-# Don't Remove Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit @Syd_Xyz
+# Ask Doubt on telegram @Bot_cracker
+
 
 
 import re
@@ -163,6 +163,6 @@ LOG_STR += (f"MAX_LIST_ELM Found, long list will be shortened to first {MAX_LIST
 LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
 
 
-# Don't Remove Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit @Syd_Xyz
+# Ask Doubt on telegram @Bot_cracker
+
