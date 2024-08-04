4d848e9889808e26c07e24d8370eae522854a5f2
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2024-08-04 07:37:32 +0530

    Update settings.py

--- MrSyDClone/settings.py
+++ MrSyDClone/settings.py
@@ -1,1 +1,16 @@
-hi
+from pyrogram import Client, filters
+import asyncio
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+
+@Client.on_callback_query(filters.regex(r'^settings'))
+async def settings_query(bot, query):
+  user_id = query.from_user.id
+  i, type = query.data.split("#")
+  buttons = [[InlineKeyboardButton('🔙 Back', callback_data="settings#main")]]
+  
+  if type=="main":
+     await query.message.edit_text(
+       "<b>Change Your Settings As Your Wish</b>",
+       reply_markup=main_buttons())
+       
