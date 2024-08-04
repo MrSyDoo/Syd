5367d2298a7703d9b5949950bcef635954687185
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2024-08-04 07:39:55 +0530

    Update settings.py

--- MrSyDClone/settings.py
+++ MrSyDClone/settings.py
@@ -3,6 +3,22 @@ import asyncio
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from MrSyDClone.database.clone_bot_userdb import clonedb
 
+
+
+
+
+@Client.on_message(filters.private & filters.command(['edit']))
+async def settings(client, message):
+    me = await client.get_me()
+    owner = await db.get_bot(me.id)
+    if owner["user_id"] != message.from_user.id:
+        return
+    text="<b>Cʀᴇᴀᴛᴇ Yᴏᴜʀ Oᴡɴ Bᴏᴛ Δɴᴅ Eᴅɪᴛ ɪᴛ ᴀꜱ ʏᴏᴜʀ ᴡɪꜱʜ ᴍᴀʜɴ.....⚡</b>"
+    await message.reply_text(
+        text=text,
+        reply_markup=main_buttons(),
+        quote=True
+    )
 @Client.on_callback_query(filters.regex(r'^settings'))
 async def settings_query(bot, query):
   user_id = query.from_user.id
