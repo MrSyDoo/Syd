f448f0a729de6e8000d2a10bd2064747e85dc97e
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2024-08-04 06:52:15 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -57,7 +57,7 @@ async def start():
             load = importlib.util.module_from_spec(spec)
             spec.loader.exec_module(load)
             sys.modules["plugins." + plugin_name] = load
-            print("Tech VJ Imported => " + plugin_name)
+            print("Mr SyD Imported => " + plugin_name)
     if ON_HEROKU:
         asyncio.create_task(ping_server())
     b_users, b_chats = await db.get_banned()
