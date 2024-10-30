eb13677daefaf1efd84cf4dccfc48d7286a36ee2
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2024-10-30 07:37:19 +0530

    Update commands.py

--- MrSyDClone/commands.py
+++ MrSyDClone/commands.py
@@ -30,7 +30,7 @@ async def start(client, message):
             buttons.append([InlineKeyboardButton('🍿 ᴊᴏɪɴ ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ 🍿', url=up)])
         reply_markup = InlineKeyboardMarkup(buttons)
         syd = cd["strtsyd"]
-        await message.reply(script.syd.format(message.from_user.mention if message.from_user else message.chat.title, me.username, me.first_name), reply_markup=reply_markup)
+        await message.reply(syd.format(message.from_user.mention if message.from_user else message.chat.title, me.username, me.first_name), reply_markup=reply_markup)
         return 
     if not await clonedb.is_user_exist(me.id, message.from_user.id):
         await clonedb.add_user(me.id, message.from_user.id)
