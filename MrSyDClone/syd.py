6793728a0b2b88654f1385b0c458fe47672625c5
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2024-11-04 18:58:46 +0530

    Update syd.py

--- MrSyDClone/syd.py
+++ MrSyDClone/syd.py
@@ -10,47 +10,17 @@ syd_db = None
 REQUEST_TO_JOIN_MODE = True
 AUTH_CHANNEL = None 
 
-async def setup_join_db(bot_id, auth_channel):
-    global syd_db, AUTH_CHANNEL
-    syd_db = JoinReqs(bot_id, auth_channel)
-    AUTH_CHANNEL = auth_channel
-
-async def syd_subscribed(bot, query):
-    global AUTH_CHANNEL
-    if syd_db is None or AUTH_CHANNEL is None:
-        me = await bot.get_me()
-        cd = await db.get_bot(me.id)
-        AUTH_CHANNEL = cd["fsub"]
-        await setup_join_db(bot_id=me.id, auth_channel=AUTH_CHANNEL)
-        
-    if REQUEST_TO_JOIN_MODE and syd_db.isActive():
-        try:
-            user = await syd_db.get_user(query.from_user.id)
-            if user and user["user_id"] == query.from_user.id:
-                return True
-            else:
-                try:
-                    user_data = await bot.get_chat_member(AUTH_CHANNEL, query.from_user.id)
-                except UserNotParticipant:
-                    pass
-                except Exception as e:
-                    logger.exception(e)
-                else:
-                    if user_data.status != enums.ChatMemberStatus.BANNED:
-                        return True
-        except Exception as e:
-            logger.exception(e)
-            return False
+async def is_req_subscribed(bot, query):
+    if await db.find_join_req(query.from_user.id):
+        return True
+    try:
+        user = await bot.get_chat_member(AUTH_CHANNEL, query.from_user.id)
+    except UserNotParticipant:
+        pass
+    except Exception as e:
+        logger.exception(e)
     else:
-        try:
-            user = await bot.get_chat_member(AUTH_CHANNEL, query.from_user.id)
-        except UserNotParticipant:
-            pass
-        except Exception as e:
-            logger.exception(e)
-        else:
-            if user.status != enums.ChatMemberStatus.BANNED:
-                return True
-        return False
+        if user.status != enums.ChatMemberStatus.BANNED:
+            return True
 
-
+    return False
