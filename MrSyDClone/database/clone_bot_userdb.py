8e7e403606688c31a813962aee0de60bc2cab153
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2024-11-04 06:59:43 +0530

    Update clone_bot_userdb.py

--- MrSyDClone/database/clone_bot_userdb.py
+++ MrSyDClone/database/clone_bot_userdb.py
@@ -1,45 +1,58 @@
 import motor.motor_asyncio
 from info import CLONE_DATABASE_URI, DATABASE_NAME
+import motor.motor_asyncio
 from info import AUTH_CHANNEL, DATABASE_URI
 
 class JoinReqs:
+    def __init__(self, bot_id, auth_channel):
+        self.bot_id = bot_id
+        self.auth_channel = auth_channel
 
-    def __init__(self):
         if DATABASE_URI:
             self.client = motor.motor_asyncio.AsyncIOMotorClient(DATABASE_URI)
             self.db = self.client["JoinReqs"]
-            self.col = self.db[str(AUTH_CHANNEL)]
+            self.col = self.db[str(self.auth_channel)]
         else:
             self.client = None
             self.db = None
             self.col = None
 
     def isActive(self):
-        if self.client is not None:
-            return True
-        else:
-            return False
+        return self.client is not None
 
     async def add_user(self, user_id, first_name, username, date):
         try:
-            await self.col.insert_one({"_id": int(user_id),"user_id": int(user_id), "first_name": first_name, "username": username, "date": date})
+            await self.col.insert_one({
+                "_id": f"{self.bot_id}_{user_id}",  # Unique ID with bot_id
+                "bot_id": self.bot_id,
+                "user_id": int(user_id),
+                "first_name": first_name,
+                "username": username,
+                "date": date
+            })
         except:
             pass
 
-    async def get_user(self, user_id):
-        return await self.col.find_one({"user_id": int(user_id)})
-
+    async def get_user(self, user_id, bot_id=None):
+        return await self.col.find_one({"user_id": int(user_id), "bot_id": self.bot_id})
+        
     async def get_all_users(self):
-        return await self.col.find().to_list(None)
+        # Include bot_id in the filter
+        return await self.col.find({"bot_id": self.bot_id}).to_list(None)
 
     async def delete_user(self, user_id):
-        await self.col.delete_one({"user_id": int(user_id)})
+        # Include bot_id in the filter
+        await self.col.delete_one({"user_id": int(user_id), "bot_id": self.bot_id})
 
     async def delete_all_users(self):
-        await self.col.delete_many({})
+        # Include bot_id in the filter
+        await self.col.delete_many({"bot_id": self.bot_id})
 
-    async def get_all_users_count(self):
-        return await self.col.count_documents({})
+    async def get_all_users_count(self): 
+        # Include bot_id in the filter
+        return await self.col.count_documents({"bot_id": self.bot_id})
+
+
 class Database:
     
     def __init__(self, uri, database_name):
