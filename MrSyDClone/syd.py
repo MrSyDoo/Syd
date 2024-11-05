7023d3c219f43f0fcf4641f45b814dd8be548fdd
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2024-11-05 07:30:13 +0530

    Update syd.py

--- MrSyDClone/syd.py
+++ MrSyDClone/syd.py
@@ -6,12 +6,12 @@ from MrSyDClone.database.clone_bot_userdb import clonedb as syd
 from database.users_chats_db import db
 
 
-syd_db = None  
-REQUEST_TO_JOIN_MODE = True
-AUTH_CHANNEL = None 
+
 
 async def syd_subscribed(bot, query):
     me = await client.get_me()
+    cd = await db.get_bot(me.id)
+    MR_SYD = cd["fsub"]
     mssydtg = me.id + query.from_user.id
     if await syd.find_join_req(mssydtg):
         return True
@@ -24,5 +24,5 @@ async def syd_subscribed(bot, query):
     else:
         if user.status != enums.ChatMemberStatus.BANNED:
             return True
-
+ 
     return False
