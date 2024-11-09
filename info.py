890f36a9f6588af681dd44aefe2f2fc59b5804be
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2024-11-09 18:46:56 +0530

    Update info.py

--- info.py
+++ info.py
@@ -99,7 +99,7 @@ NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", True))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
 # Token Verification Info :
-VERIFY = bool(environ.get('VERIFY', False))
+VERIFY = bool(environ.get('VERIFY', True))
 VERIFY_SECOND_SHORTNER = bool(environ.get('VERIFY_SECOND_SHORTNER', False))
 VERIFY_SHORTLINK_URL = environ.get('VERIFY_SHORTLINK_URL', 'api.shareus.io')
 VERIFY_SHORTLINK_API = environ.get('VERIFY_SHORTLINK_API', 'https://api.shareus.io/easy_api?key=X9FCk9uYkiYIRTJJoaAoQ4My1M02&link=https://shareus.io')
