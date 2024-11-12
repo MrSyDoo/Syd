3d6e2c1209bdc180ed7be4af04c13739d16236f9
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2024-11-12 07:28:18 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -58,8 +58,12 @@ async def save_file(media):
     # TODO: Find better way to get same file_id for same media to avoid duplicates
     file_id, file_ref = unpack_new_file_id(media.file_id)
     file_name = re.sub(r"(_|\-|\.|\+)", " ", str(media.file_name))
+    if data_size > 503316480:
+        SyDMedia = Media2
+    else:
+        SyDMedia = Media
     try:
-        file = Media(
+        file = SyDMedia(
             file_id=file_id,
             file_ref=file_ref,
             file_name=file_name,
@@ -72,20 +76,27 @@ async def save_file(media):
         logger.exception('Error occurred while saving file in database')
         return False, 2
     else:
+        if SyDMedia == Media2:
+            found = {'file_id': file_id}
+            check = Media.find_one(found)
+            if check:
+                print(f"{file_name} is already saved.")
+                return False, 0
+                
         try:
             await file.commit()
         except DuplicateKeyError:      
-            logger.warning(
-                f'{getattr(media, "file_name", "NO_FILE")} is already saved in database'
-            )
-
+            print(f"{file_name} is already saved.")
             return False, 0
         else:
-            logger.info(f'{getattr(media, "file_name", "NO_FILE")} is saved to database')
+            print(f"{file_name} is saved to database.")
             return True, 1
 
-
-
+async def get_file_db_size():
+    data_size = (await db.command("dbstats"))['dataSize']
+    data_size2 = (await sec_db.command("dbstats"))['dataSize']
+    return data_size, data_size2
+    
 async def get_search_results(chat_id, query, file_type=None, max_results=10, offset=0, filter=False):
     """For given query return (results, next_offset)"""
     if chat_id is not None:
@@ -127,19 +138,26 @@ async def get_search_results(chat_id, query, file_type=None, max_results=10, off
     if file_type:
         filter['file_type'] = file_type
 
-    total_results = await Media.count_documents(filter)
+    result1 = await Media.count_documents(filter)
+    result2 = await Media2.count_documents(filter)
+    total_results = result1 + result2
     next_offset = offset + max_results
 
     if next_offset > total_results:
         next_offset = ''
 
-    cursor = Media.find(filter)
+    cursor1 = Media.find(filter)
+    cursor2 = Media2.find(filter)
     # Sort by recent
-    cursor.sort('$natural', -1)
+    cursor1.sort('$natural', -1)
+    cursor2.sort('$natural', -1)
     # Slice files according to offset and max results
-    cursor.skip(offset).limit(max_results)
+    cursor1.skip(offset).limit(max_results)
+    cursor2.skip(offset).limit(max_results)
     # Get list of files
-    files = await cursor.to_list(length=max_results)
+    files1 = await cursor1.to_list(length=max_results)
+    files2 = await cursor2.to_list(length=max_results)
+    files = files1 + files2
 
     return files, next_offset, total_results
 
@@ -170,20 +188,29 @@ async def get_bad_files(query, file_type=None, filter=False):
     if file_type:
         filter['file_type'] = file_type
 
-    total_results = await Media.count_documents(filter)
-
-    cursor = Media.find(filter)
+    result1 = await Media.count_documents(filter)
+    result2 = await Media2.count_documents(filter)
+    total_results = result1 + result2
+    
+    cursor1 = Media.find(filter)
+    cursor2 = Media2.find(filter)
     # Sort by recent
-    cursor.sort('$natural', -1)
+    cursor1.sort('$natural', -1)
+    cursor2.sort('$natural', -1)
     # Get list of files
-    files = await cursor.to_list(length=total_results)
-
+    files1 = await cursor1.to_list(length=max_results)
+    files2 = await cursor2.to_list(length=max_results)
+    files = files1 + files2
+    
     return files, total_results
 
 async def get_file_details(query):
     filter = {'file_id': query}
     cursor = Media.find(filter)
     filedetails = await cursor.to_list(length=1)
+    if not filedetails:
+        cursor1 = Media2.find(filter)
+        filedetails = await cursor1.to_list(length=1)
     return filedetails
 
 
