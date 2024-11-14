59a9b66a94db030974a1cdd1a81b32bc6bb07701
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2024-11-14 07:25:50 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -58,8 +58,9 @@ async def save_file(media):
     # TODO: Find better way to get same file_id for same media to avoid duplicates
     file_id, file_ref = unpack_new_file_id(media.file_id)
     file_name = re.sub(r"(_|\-|\.|\+)", " ", str(media.file_name))
-    data_size = (await db.command("dbstats"))['dataSize']
-    print(f"{data_size} •")
+    result = await db.command("dbstats")
+    data_size = result['dataSize']
+    print(f"{data_size} • 503316480")
   #  if data_size > 503316480:
     #    SyDMedia = Media2
   #  else:
