0c73f9ae5c66698fbbb056210508df5fa1c30c28
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2024-11-25 07:23:46 +0530

    Update users_chats_db.py

--- database/users_chats_db.py
+++ database/users_chats_db.py
@@ -343,7 +343,7 @@ class Database:
         
     async def get_verified(self, id):
         default = {
-            'date': "2024-11-14",
+            'date': "2024-12-2",
             'time': "23:59:59"
         }
         user = await self.col.find_one({'id': int(id)})
